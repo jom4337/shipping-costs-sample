8e48acdebcac4fe388d94a83f843fff975e1f18b
irkas <irina@irka.org>
2017-02-15 16:35:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500,
            'IE4D':'Island Echos 4D', 'IE2A':'Island Echos 2A'
            
            }
