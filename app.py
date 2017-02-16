91c54b1061fd8882027c1ad37088f08b85188176
irkas <irina@irka.org>
2017-02-15 18:37:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("unit-name")
 
-    cost = {'ie4d':'Island Echo 4 D', 'ie2a':'Island Echo 2 A', 'North America':200, 'South America':300, 
-            'Asia':'Island Echo 4 D', 'Africa':500, 'Europe':4 }
+    cost = {'ie4d':'Island Echo 4 D', 'ie2a':'Island Echo 2 A' }
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
