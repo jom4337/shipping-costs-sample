f239351f8a402b0facaebb4d33c8606ff0cbd9c7
irkas <irina@irka.org>
2017-02-15 18:40:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -53,42 +53,7 @@ def makeWebhookResult(req):
 
 
 
-@app.route('/webhook2', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = makeWebhookResult(req)
-
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def makeWebhookResult(req):
-    if req.get("result").get("action") != "unit.name":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("unit-name")
-
-    cost = {'ie4d':'Island Echo 4 D', 'ie2a':'Island Echo 2 A' }
-
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
-    }
 
 
 
