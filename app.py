5c6490a8b586c5a81599ceb93a1eba6bb18ff59e
irkas <irina@irka.org>
2017-02-18 17:33:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -89,9 +89,6 @@ def makeWebhookResult(req):
                                                                       }}],
         "source": "lodgekit-stay"
         }
-    
-    #speech="Welcome to "+unit+" ."+"  I am Leelu, how can I help.  For example. say - wifi, or contact host, or address, or checkout"
-
         
     print("Response:")
     print(speech)
