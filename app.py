08fd215eb4f5681431dea79f2687d54e554326b0
irkas <irina@irka.org>
2017-02-17 23:54:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,20 @@ def makeWebhookResult(req):
     }
 
 
+if var == 200:
+   print "1 - Got a true expression value"
+   print var
+elif var == 150:
+   print "2 - Got a true expression value"
+   print var
+elif var == 100:
+   print "3 - Got a true expression value"
+   print var
+else:
+   print "4 - Got a false expression value"
+   print var
+
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
